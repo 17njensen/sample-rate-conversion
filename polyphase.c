0cd5575af27db66cd01f1e7caf28ed58cfab40ef
Nikolas Jensen <17njensen1@gmail.com>
2021-11-18 19:40:23 -0700

    polyphase pt1
    I made the awful part, now to debug

--- polyphase.c
+++ polyphase.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-// #include "filter.h"
-// #include "filter_scale.h"
 #include "filter_3.h"
 #include "cos_16.h"
 #include "cos_8.h"
@@ -68,16 +66,28 @@ float* upsample(int l, float* x, float* xl, int Lzl, int Lx, int Lh){
     printf("Start Upsampling\n");
     int j = 0;
     int i;
-    for(i = Lh-1; i < (Lzl-Lh-1); i++){
-    // for(int i = Lh-1, j = 0; i < (Lzl-Lh-1), j < Lx; i++, j++){ 
-        // printf("i = %d\tj = %d\n",i,j);
-        if(i%(L) == 0){
-            // printf("i = %d, j = %d\n",i,j);
-            xl[i] = x[j];
-            j++;
+    if(Lh == NULL){
+        for(i = 0; i < (Lzl); i++){
+            if(i%(L) == 0){
+                // printf("i = %d, j = %d\n",i,j);
+                xl[i] = x[j];
+                j++;
+            }
         }
+        printf("i = %d, j = %d\n",i,j);
     }
-    printf("i = %d, j = %d\n",i,j);
+    else{
+        for(i = Lh-1; i < (Lzl-Lh-1); i++){
+        // for(int i = Lh-1, j = 0; i < (Lzl-Lh-1), j < Lx; i++, j++){ 
+            // printf("i = %d\tj = %d\n",i,j);
+            if(i%(L) == 0){
+                // printf("i = %d, j = %d\n",i,j);
+                xl[i] = x[j];
+                j++;
+            }
+        }
+        printf("i = %d, j = %d\n",i,j);
+    }
     // printf("%d\n",sizeof(xl)); //i = 144175      j = 47899
     return xl;
 }
@@ -90,15 +100,25 @@ float* upsample(int l, float* x, float* xl, int Lzl, int Lx, int Lh){
  * @param Lx is the length of the input
  * @param Ly is the length of our output
  */
-float* downsample(int m, float* x, float* y, int Lx, int Ly){
+float* downsample(int m, float* x, float* y, int Lx, int Ly, int Lh){
     printf("Start Downsampling\n");
     int j = 0;
-    for(int i = 0; i < Lx; i++){
-    // for(int i = 0, j = 0; i < Lx, j < Ly; i++, j++){//grab every M-1 sample, store in every y[j]
-        if(i%(m) == 0){
-            // printf("i = %d, j = %d\n",i,j);
-            y[j] = x[i];
-            j++;
+    if(Lh == NULL){
+        for(int i = 0; i < Lx; i++){
+            if(i%(m) == 0){
+                // printf("i = %d, j = %d\n",i,j);
+                y[j] = x[i];
+                j++;
+            }
+        }
+    }
+    else{
+        for(int i = Lh-1; i < (Lx-Lh-1); i++){
+            if(i%(m) == 0){
+                // printf("i = %d, j = %d\n",i,j);
+                y[j] = x[i];
+                j++;
+            }
         }
     }
     return y;
@@ -118,47 +138,80 @@ float* conv(float* x, float* h, float* y, int Lx, int Lh, int Ly){
     printf("Lh = %d, Lx = %d, Lv = %d\n", Lh, Lx, Ly); 
     int j;
     for (int i = 0; i < Ly; i++) { 
-        // printf("check %d\n", i);
         for (j = 0; j < Lh; j++) { 
             y[i] += h[j] * x[i + j]; //multiply and accumulate (MAC) 
-            // printf("x[%d] = %f\th[%d] = %f\n",i,x[i+j],i,h[i]);
-            //impulse is assumed to be in time reverse order 
         } 
-        //printf("i = %d\t j = %d\n", i, j); //i = 61703        j = 479
     }
     return y;
 }
 
-//dont know if this is needed yet
 /**
- * @brief Delay function?
+ * @brief Delay function. Delays the whole input by delay_val
  * @param input is input
  * @param output is output 
  * @param in_length is input length
  * @param out_length is output length
  * @param delay_val is how much the input gets delayed by
  */
-float* delay(float* input, float* output, int in_length, int out_length, int delay_val){
+float* shift(float* input, float* output, int in_length, int out_length, int delay_val){
     for(int i = 0; i < in_length; i++){
-        output[i] = input[i-delay_val]; //for out example, I believe we are seperating the input into 3 different outcomes 
+        if(i+delay_val < out_length && i+delay_val > 0){
+            output[i+delay_val] = input[i];
+        }
+        else if(i+delay_val == out_length){
+            return output;
+        }
     }
 }
 
 /**
  * @brief fetches r functions for polyphase
+ * @param m M
  * @param r0 r0 input
  * @param r1 r1 input
  * @param r2 r2 input
  * @param h impulse
  * @param Lh impulse length
  */
-float* fetch_r(int l, float* r0, float* r1, float* r2, float* h, int Lh){
+float* fetch_r_into_3(float* r0, float* r1, float* r2, float* h, int Lh){
+    int j = 0, k = 0, n = 0;
     for(int i = 0; i < Lh; i++){
-        r0[i] = h[i*l + 0];
-        r1[i] = h[i*l + 1];
-        r2[i] = h[i*l + 2];
+        if(i%3 == 0){ //n = 0, 3, 6, ...
+            r2[j] = h[i];
+            j++;
+        }
+        else if(i%3 == 1){ //n = 1, 4, 7, ...
+            r1[k] = h[i];
+            k++;
+        }
+        else if(i%3 == 2){ //n = 2, 5, 8, ...
+            r0[n] = h[i];
+            n++;
+        }
     }
     return r0,r1,r2;
+}
+/**
+ * @brief Splits R into two Rs
+ * @param input input to be split
+ * @param r0 first output
+ * @param r1 second output
+ * @param length length of the input
+ */
+float* split_r_in_2(float* input, float* r0, float* r1, int length){
+    int j = 0;
+    int k = 0;
+    for(int i = 0; i < length; i++){
+        if(i%2 == 0){ //n = 0, 2, 4, ...
+            r1[j] = input[i];
+            j++;
+        }
+        else if(i%2 == 1){ //n = 1, 3, 5, ...
+            r0[k] = input[i];
+            k++;
+        }
+    }
+    return r0, r1;
 }
 
 void main(int argc, char** argv){
@@ -194,21 +247,146 @@ void main(int argc, char** argv){
     int Lz = Lxl + 2*(Lh-1); //length output for upsampled
     int Lv = Lxl + (Lh-1); //length of filtered upsampled signal 
     int Ly = Lv / M; //final output length
-    printf("Lh = %d, Lx = %d, Ly = %d, Lv = %d, Lzl = %d\n", Lh, Lx, Ly, Lv, Lz); 
+    // printf("Lh = %d, Lx = %d, Ly = %d, Lv = %d, Lzl = %d\n", Lh, Lx, Ly, Lv, Lz); 
     float* x = calloc(sizeof(float), Lx); //allocate data for file store 
-    float* v = calloc(sizeof(float), Lz);
-    float* y = calloc(sizeof(float), Ly); 
-    float* xl = calloc(sizeof(float), Lz); 
+    // float* v = calloc(sizeof(float), Lz);
+    // float* y = calloc(sizeof(float), Ly); 
+    // float* xl = calloc(sizeof(float), Lz); 
     
-    while (!feof(fx)) { // might need to adjust to account for convolution after we upsample. So maybe have size be just h1.d0 instead of + Lh-1 when reading
-        // fread((x + Lh - 1), sizeof(float), Lx, fx); //pulls in data when H0 is present, hence x+Lh-1 
+    while (!feof(fx)) { 
         fread(x, sizeof(float), Lx, fx);
     } 
 
-    float* r0 = calloc(sizeof(float), Lh/L);//size needs to be changed?
-    float* r1 = calloc(sizeof(float), Lh/L);
-    float* r2 = calloc(sizeof(float), Lh/L);
-    r0,r1,r2 = fetch_r(L, r0, r1, r2, h, Lh); //could modulate this better so less hard coding if used later
+    int Lrk = Lh/L;
+    int Lrkk = Lrk/M;
+    float* r0 = calloc(sizeof(float), Lrk);//size needs to be changed?
+    float* r1 = calloc(sizeof(float), Lrk);
+    float* r2 = calloc(sizeof(float), Lrk);
+    r0,r1,r2 = fetch_r_into_3(r0, r1, r2, h, Lh); //could modulate this better so less hard coding if used later
+    float* r00 = calloc(sizeof(float), Lrkk);
+    float* r01 = calloc(sizeof(float), Lrkk);
+    r00, r01 = split_r_in_2(r0, r00, r01, Lrk);
+    float* r10 = calloc(sizeof(float), Lrkk);
+    float* r11 = calloc(sizeof(float), Lrkk);
+    r10, r11 = split_r_in_2(r1, r10, r11, Lrk);
+    float* r20 = calloc(sizeof(float), Lrkk);
+    float* r21 = calloc(sizeof(float), Lrkk);
+    r20, r21 = split_r_in_2(r2, r20, r21, Lrk);
+    free(r0);
+    free(r1);
+    free(r2);
+
+    int Ld_pad = (Lx/2)+2*(Lh-1);
+    int Lvk = (Lx/2)+(Lh-1); 
+
+
+    //1.)
+    float* x0_1 = calloc(sizeof(float), Lx);
+    float* x0_2 = calloc(sizeof(float), Lx);
+    x0_1 = shift(x, x0_1, Lx, Lx, 1, -2);
+    x0_2 = shift(x0_1, x0_2, Lx, Lx, 1, 1);
+    float* xd0_1 = calloc(sizeof(float), Ld_pad);
+    float* xd0_2 = calloc(sizeof(float), Ld_pad);
+    xd0_1 = downsample(M, x0_1, xd0_1, Lx, Ld_pad, Lh);
+    xd0_2 = downsample(M, x0_2, xd0_2, Lx, Ld_pad, Lh);
+    float* v0_1 = calloc(sizeof(float), Lvk);
+    float* v0_2 = calloc(sizeof(float), Lvk);
+    v0_1 = conv(xd0_1, r00, v0_1, Ld_pad, Lrkk, Lvk);
+    v0_2 = conv(xd0_2, r01, v0_2, Ld_pad, Lrkk, Lvk);
+    float* v_0 = calloc(sizeof(float), Lvk);
+    for(int i = 0; i < Lvk; i++){
+        v_0[i] = v0_1[i] + v0_2[i];
+    }
+    float* yu_0 = calloc(sizeof(float), Lvk*3);
+    yu_0 = upsample(L, v_0, yu_0, 3*Lvk, Lvk, NULL);
+
+    float* y_0 = calloc(sizeof(float), 3*Lvk);
+    y_0 = shift(yu_0, y_0, 3*Lvk, 3*Lvk, 1);
+    free(x0_1);
+    free(x0_2);
+    free(xd0_1);
+    free(xd0_2);
+    free(v0_1);
+    free(v0_2);
+    free(v_0);
+    free(yu_0);
+    free(r00);
+    free(r01);
+    
+
+    //2.)
+    float* x1_1 = calloc(sizeof(float), Lx);
+    float* x1_2 = calloc(sizeof(float), Lx);
+    x1_1 = shift(x, x1_1, Lx, Lx, 1, -2);
+    x1_2 = shift(x1_1, x1_2, Lx, Lx, 1, 1);
+    float* xd1_1 = calloc(sizeof(float), Ld_pad);
+    float* xd1_2 = calloc(sizeof(float), Ld_pad);
+    xd1_1 = downsample(M, x1_1, xd1_1, Lx, Ld_pad, Lh);
+    xd1_2 = downsample(M, x1_2, xd1_2, Lx, Ld_pad, Lh);
+    float* v1_1 = calloc(sizeof(float), Lvk);
+    float* v1_2 = calloc(sizeof(float), Lvk);
+    v1_1 = conv(xd1_1, r10, v1_1, Ld_pad, Lrkk, Lvk);
+    v1_2 = conv(xd1_2, r11, v1_2, Ld_pad, Lrkk, Lvk);
+    float* v_1 = calloc(sizeof(float), Lvk);
+    for(int i = 0; i < Lvk; i++){
+        v_1[i] = v1_1[i] + v1_2[i];
+    }
+    float* y_1 = calloc(sizeof(float), Lvk*3);
+    y_1 = upsample(L, v_1, y_1, 3*Lvk, Lvk, NULL);
+    free(x1_1);
+    free(x1_2);
+    free(xd1_1);
+    free(xd1_2);
+    free(v1_1);
+    free(v1_2);
+    free(v_1);
+    free(r10);
+    free(r11);
+    
+
+
+    //3.)
+    float* x2_1 = calloc(sizeof(float), Lx);
+    float* x2_2 = calloc(sizeof(float), Lx);
+    x2_1 = shift(x, x2_1, Lx, Lx, 1, -2);
+    x2_2 = shift(x2_1, x2_2, Lx, Lx, 1, 1);
+    float* xd2_1 = calloc(sizeof(float), Ld_pad);
+    float* xd2_2 = calloc(sizeof(float), Ld_pad);
+    xd2_1 = downsample(M, x2_1, xd2_1, Lx, Ld_pad, Lh);
+    xd2_2 = downsample(M, x2_2, xd2_2, Lx, Ld_pad, Lh);
+    float* v2_1 = calloc(sizeof(float), Lvk);
+    float* v2_2 = calloc(sizeof(float), Lvk);
+    v2_1 = conv(xd2_1, r20, v2_1, Ld_pad, Lrkk, Lvk);
+    v2_2 = conv(xd2_2, r21, v2_2, Ld_pad, Lrkk, Lvk);
+    float* v_2 = calloc(sizeof(float), Lvk);
+    for(int i = 0; i < Lvk; i++){
+        v_2[i] = v2_1[i] + v2_2[i];
+    }
+    float* y_2 = calloc(sizeof(float), Lvk*3);
+    y_2 = upsample(L, v_2, y_2, 3*Lvk, Lvk, NULL);
+    free(x2_1);
+    free(x2_2);
+    free(xd2_1);
+    free(xd2_2);
+    free(v2_1);
+    free(v2_2);
+    free(v_2);
+    free(r20);
+    free(r21);
+
+
+    //finish section
+    float* y_01 = calloc(sizeof(float), 3*Lvk);
+    for(int i = 0; i < 3*Lvk; i++){
+        y_01[i] = y_0[i] + y_1[i];
+    }
+    float* y01 = calloc(sizeof(float), 3*Lvk);
+    y01 = shift(y_01, y01, 3*Lvk, 3*Lvk, 1);
+    float* y = calloc(sizeof(float), 3*Lvk);
+    for(int i = 0; i < 3*Lvk; i++){
+        y[i] = y_2[i] + y01[i];
+    }
+    fwrite(y, sizeof(float), 3*Lvk, fy);
 
     /*
     //--------------------------------------------standard form--------------------------------------------------
